631c18de0bf3bccfd2f3d3c7b0a731369398d3a2
Angel Macias <macia324@coyote.csusb.edu>
2017-11-20 19:51:59 -0800

    AddingProjectileVersion2
    Re-worked the projectile version done by Jordan. His version only created bullets. Added ability to:
    1) Control how long bullets stay in scene
    2) Shooting left and right
    3) Minor performance tweaks.
    All updated code is commeneted.

--- 2D Game Project/Practice 2D/Assets/Scripts/PlayerController.cs
+++ 2D Game Project/Practice 2D/Assets/Scripts/PlayerController.cs
@@ -8,16 +8,19 @@ public class PlayerController : MonoBehaviour {
 	public float speedForce = 50f;
 	public Vector2 jumpVector;
 	public Vector2 jumpVector2;
+	bool facingRight;
 	public bool isGrounded;
-	float speed;
+	public float speed;
 
 	public Transform grounder;
 	public float radiuss;
 	public LayerMask ground;
 
 	Animator anim;
-
+	Rigidbody2D rb;
 	public float health;
+	public GameObject bulletRight, bulletLeft;
+	Transform firePosition;
 
 	void Awake()
 	{
@@ -26,14 +29,19 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
+		rb = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
 		health = GetComponent<HealthScript> ().health;
+		firePosition = transform.Find ("firePosition");
+		facingRight = true;
+		isGrounded = true;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		Movement ();
+		Flip ();
 	}
 		
 	/**********************************************************
@@ -45,18 +53,23 @@ public class PlayerController : MonoBehaviour {
 	 * ******************************************************/
 	void Movement()
 	{
-		if (Input.GetKey (KeyCode.A)) {
-			speed = isGrounded ? speedForce : speedForce*0.8f;
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-speed, GetComponent<Rigidbody2D> ().velocity.y);
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			speed = isGrounded ? -speedForce : -speedForce*0.8f;
+			rb.velocity = new Vector2 (speed, rb.velocity.y);
 			transform.localScale = new Vector3 (-3, 4, 1);
 			anim.SetInteger ("AnimationState", 1);
-		} else if (Input.GetKey (KeyCode.D)) {
+		} 
+		else if (Input.GetKey (KeyCode.D)) 
+		{
 			speed = isGrounded ? speedForce : speedForce*0.8f;
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed, GetComponent<Rigidbody2D> ().velocity.y);
+			rb.velocity = new Vector2 (speed, rb.velocity.y);
 			transform.localScale = new Vector3 (3, 4, 1);
 			anim.SetInteger ("AnimationState", 1);
-		} else if (isGrounded)
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, GetComponent<Rigidbody2D> ().velocity.y);
+		} 
+		else if (isGrounded)
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+
 		anim.SetInteger ("AnimationState", 0);
 
 
@@ -64,14 +77,19 @@ public class PlayerController : MonoBehaviour {
 
 		if(Input.GetKey(KeyCode.W) && isGrounded==true) 
 		{
-			GetComponent<Rigidbody2D>().AddForce (jumpVector, ForceMode2D.Force);
+			rb.AddForce (jumpVector, ForceMode2D.Force);
 		}
 		else if (Input.GetKey(KeyCode.W))
 		{
-			GetComponent<Rigidbody2D>().AddForce (jumpVector2, ForceMode2D.Force);
+			rb.AddForce (jumpVector2, ForceMode2D.Force);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Fire ();
 		}
 	}
-
+		
 	/**********************************************************
 	 * updateHealth: Sets the current health equal to the
 	 * health located in HealthScript.
@@ -90,6 +108,38 @@ public class PlayerController : MonoBehaviour {
 		if(other.gameObject.name == "Spike")
 		{
 			updateHealth ();
+		}
+	}
+
+	/**********************************************************
+	 * Uses the facingRight boolean to determine if we are
+	 * instantiating a bullet going left or going right.
+	 * *******************************************************/
+	void Fire()
+	{
+		if(facingRight) 
+			Instantiate (bulletRight, firePosition.position, Quaternion.identity);
+		else
+			Instantiate (bulletLeft, firePosition.position, Quaternion.identity);
+	}
+
+	/**********************************************************
+	 * This flip method is used to determine if we are facing
+	 * right or facing left. Facing left is set when
+	 * 1) Speed > 0 and we are facing left OR
+	 * 2) Speed < 0 and we are facing right
+	 * Uses a temp Vector variable to temporarily store the 
+	 * local transform variables and then set it to the 
+	 * opposite value.
+	 * ********************************************************/
+	void Flip()
+	{
+		if (speed > 0 && !facingRight || speed < 0 && facingRight) 
+		{
+			facingRight = !facingRight;
+			Vector3 temp = transform.localScale;
+			temp.x *= -1;
+			transform.localScale = temp;
 		}
 	}
 }
